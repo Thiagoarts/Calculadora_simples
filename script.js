27c392d471ba7435bca45075a0a2c3081b65c335
Thiago Silva <thiago.sbenedito@icloud.com>
2025-07-08 09:42:29 -0300

    personalizando calculadora e adicionando os botões dos numeros e personalizando visual.

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-function pegarValores(){
-    const n1 = Number(document.getElementById("num1").value);
-    const n2 = Number(document.getElementById("num2").value);
-    return [n1, n2];
+function inserir(valor) {
+  document.getElementById("display").value += valor;
 }
 
-function mostrarResultado(texto){
-    document.getElementById("resultado").innerText = texto;
+function limpar() {
+  document.getElementById("display").value = "";
 }
 
-function somar(){
-    const [n1, n2] = pegarValores();    
-    mostrarResultado(`Resultado: ${n1 + n2}`);
+function calcular() {
+  try {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+  } catch {
+    document.getElementById("display").value = "Erro";
+  }
 }
-
-function subtrair(){
-    const [n1, n2] = pegarValores();    
-    mostrarResultado(`Resultado: ${n1 - n2}`);
-}
-
-function multiplicar(){
-    const [n1, n2] = pegarValores();    
-    mostrarResultado(`Resultado: ${n1 * n2}`);
-}
-
-function dividir(){
-    const [n1, n2] = pegarValores();
-    if (n2 === 0) {
-        mostrarResultado("Erro: Divisão por zero não é permitida.");
-    } else {
-        mostrarResultado(`Resultado: ${n1 / n2}`);
-    }
-}
-
- function limparCampos(){
-    const limparBtn = document.getElementById("limparBtn");
-    limparBtn.addEventListener("click", function() {
-        document.getElementById("num1").value = "";
-        document.getElementById("num2").value = "";
-        mostrarResultado("");
-    });
-}
-limparCampos();
